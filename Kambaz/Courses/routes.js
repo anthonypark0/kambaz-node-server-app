e865f7f39ecfd5a0a888e80aaf0f4132f75c7b81
apark00 <park.ant@northeastern.edu>
2025-04-16 15:41:43 -0400

    nice

--- Kambaz/Courses/routes.js
+++ Kambaz/Courses/routes.js
@@ -1,5 +1,6 @@
 import * as dao from "./dao.js";
 import * as modulesDao from "../Modules/dao.js"
+import * as enrollmentsDao from "../Enrollments/dao.js";
 export default function CourseRoutes(app) {
   app.get("/api/courses", async (req, res) => {
     const courses = await dao.findAllCourses();
@@ -37,6 +38,10 @@ export default function CourseRoutes(app) {
 
   app.post("/api/courses", async (req, res) => {
     const course = await dao.createCourse(req.body);
+    const currentUser = req.session["currentUser"];
+    if (currentUser) {
+      await enrollmentsDao.enrollUserInCourse(currentUser._id, course._id);
+    } 
     res.json(course);
   });
  
